bcaf4d82f9c7f8ccfeda758643c9ccf1f4d24170
samhudson <samjhudson@gmail.com>
2022-01-09 19:18:21 +0000

    Added sex selection radio buttons

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const avatarImage = document.getElementById("avatar-image");
 const avatarContainer = document.getElementById("avatar-container");
 const generateNewAvatarBtn = document.getElementById("generate-new");
+const maleRadio = document.getElementById("male-radio");
+const femaleRadio = document.getElementById("female-radio");
+
+let initialLoad = true;
 
 
 
 async function getNewAvatar() {
+    let avatarName = "initialAvatar";
+    if (!initialLoad) {
+        avatarName = prompt("Please enter your avatar's name");
+    }
     const apiUrl = "https://avatars.dicebear.com/api/male/testseed.svg";
     try {
         const newAvatar = await fetch(apiUrl);
@@ -14,6 +22,7 @@ async function getNewAvatar() {
     } catch (error) {
         //catch error here
     }
+    initialLoad = false;
 
 }
 
